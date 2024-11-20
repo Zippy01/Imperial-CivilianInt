16d5874d84de083a7963022b914e3a15ba575b44
Zippy OFC <72061397+Zippy01@users.noreply.github.com>
2024-11-19 20:39:33 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zippy OFC'
+description 'Civilian Intregations for Imperial CAD'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+    'Library/colors.lua',    -- Colors library (This list may require more colors!)
+    'Library/vehicles.lua'   -- vehicles library (Update addon vehicles as needed)
+}
+server_script 'server.lua'
